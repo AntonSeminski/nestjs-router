e90edca80c7b86ee69021c55117f7e5a3ddd625a
Anton Seminski <anton_siamianski@senla.eu>
2023-01-18 21:02:18 +0300

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Req} from '@nestjs/common';
+import {HttpService} from "@nestjs/axios";
+import {firstValueFrom} from "rxjs";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly httpService: HttpService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/*')
+  async getHello(@Req() req): Promise<any> {
+    const host = req.get('Host');
+    const originUrl = req.originalUrl;
+
+    console.log('host: ', host)
+    console.log('originUrl: ', originUrl)
+    console.log(`http:/localhost:3000/${originUrl}`)
+
+    let hostname = host.substring(0, host.indexOf('.localhost'));
+    console.log(`hostname: ${hostname}`)
+
+    hostname = '127.0.01:3000' //mock
+
+    try {
+      return  (await firstValueFrom(this.httpService.get(`http:/${hostname}${originUrl}`, {}))).data;
+    } catch (e) {
+      console.log(`e: ${e}`)
+      return 'error'
+    }
   }
 }
